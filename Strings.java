37ea25e540af1f514fd1d33c9e93afad56fdfd34
Deniz <55638248+penguz@users.noreply.github.com>
2020-06-14 01:59:24 +0100

    Put these variables in order

--- Strings.java
+++ Strings.java
@@ -1,13 +1,14 @@
 public class Strings {
+    final String GREET = "Welcome to password manager v1.0";
+    final String HELP = "type 'help' for all commands.";
+    final String COMMANDS = "\nCommands:" +
+                "\n1-Add a website\n2-List all websites\n3-Delete a website\n4-List your username and password\n" +
+                "5-exit";
+    
     public void greet(){
-        final String GREET = "Welcome to password manager v1.0";
-        final String HELP = "type 'help' for all commands.";
         System.out.println(GREET + "\n" + HELP);
     }
     public void commands(){
-        final String COMMANDS = "\nCommands:" +
-                "\n1-Add a website\n2-List all websites\n3-Delete a website\n4-List your username and password\n" +
-                "5-exit";
         System.out.println(COMMANDS);
     }
 }
